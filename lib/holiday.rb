28e8478e2cf09319519ed295cc14b8d8e9c39010
Jane Justiz <janejustiz@gmail.com>
2018-06-28 20:46:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, data|
-      puts "  #{holiday.capitalize}: #{data.flatten}"
+      puts "  #{holiday.capitalize}: #{data.keys}"
     end
   end
 end
